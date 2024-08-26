b1fb2eb0c85909708082c8f9ce24da8fab696695
twangyal <tsering.w1029@gmail.com>
2024-08-25 19:19:14 -0700

    increase responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './Components/Home';
 import About from './Components/About';
 import Experience from './Components/Experience';
@@ -7,16 +7,28 @@ import Contact from './Components/Contact';
 import Sidebar from './Components/Sidebar';
 
 function App() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className="App">
-      <div className="ml-auto pr-64">
+    <div className="App flex">
+      <div className={`flex-grow ${isMobile ? 'w-full' : 'ml-auto pr-64'}`}>
         <Home />
         <About />
         <Experience />
         <Projects />
         <Contact />
       </div>
+      <div className='hidden sm:block'>
       <Sidebar />
+      </div>
     </div>
   );
 }
